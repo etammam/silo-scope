990c861c5101e68e7d5ad8c49db6336f490c64ca
Islam Abouzeid <45597470+etammam@users.noreply.github.com>
2026-05-17 07:45:04 +0200

    feat: add Serilog file sink for log persistence

--- src/Siloscope.Core/Program.cs
+++ src/Siloscope.Core/Program.cs
@@ -10,10 +10,18 @@ using Siloscope.Core.Endpoints;
 using Siloscope.Core.Interfaces;
 using StreamJsonRpc;
 
+var logPath = Path.Combine(
+    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+    "SiloScope",
+    "logs",
+    "siloscope-.log"
+);
+
 Log.Logger = new LoggerConfiguration()
     .Enrich.FromLogContext()
     .MinimumLevel.Information()
     .WriteTo.Console(theme: AnsiConsoleTheme.Literate)
+    .WriteTo.File(logPath, rollingInterval: RollingInterval.Day, retainedFileCountLimit: 7)
     .CreateLogger();
 
 var builder = Host.CreateApplicationBuilder(args);
